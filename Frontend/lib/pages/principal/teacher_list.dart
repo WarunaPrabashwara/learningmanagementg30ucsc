1295b44d53e136bdd01f32a2f2775ca7672c36cf
GayashaDS <gayashads@gmail.com>
2021-09-17 02:02:19 +0530

    update

--- Frontend/lib/pages/principal/teacher_list.dart
+++ Frontend/lib/pages/principal/teacher_list.dart
@@ -13,6 +13,47 @@ class _TeacherListState extends State<TeacherList> {
   String _status = '';
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade800,
+        title:
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Text(
+            "Teacher List".toUpperCase(),
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
+
+        body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(22.0, 15.0, 22.0, 22.0),
+              child: Center(
+                child: Text(
+                  "Teachers",
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+        ]
+      ),
 //search bar
+      )
+    ),
+    );
   }
 }
