bd2d9628376a59655f27ca068b16fc4eeb0769b3
GayashaDS <gayashads@gmail.com>
2021-09-18 01:13:36 +0530

    principal dashboard updated

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/services.dart';
 import 'package:frontend/pages/admin/dashboard.dart';
 import 'package:frontend/pages/changePassword.dart';
-import 'package:frontend/pages/principal/homepage.dart';
+import 'package:frontend/pages/principal/dashboard.dart';
+//import 'package:frontend/pages/principal/homepage.dart';
 import 'package:frontend/pages/section_head/dashboard.dart';
 import 'package:frontend/pages/student/student_home_page.dart';
 import 'package:frontend/pages/teacher/add_students_to_bucket_subjects.dart';
@@ -69,7 +70,7 @@ class MyApp extends StatelessWidget {
         '/admin/home': (BuildContext context) => new Dashboard(),
         '/section_head/home': (BuildContext context) => new DashboardSectionHead(),
         '/student/home': (BuildContext context) => new StudentHomePage(),
-        '/principal/home': (BuildContext context) => new PrincipalHomePage(),
+        '/principal/home': (BuildContext context) => new DashboardPrincipal(),
         '/login': (BuildContext context) => new loginScreen(),
 //meke okkoma danna bAha hariyata wAda karanne naha ethakota
       },
